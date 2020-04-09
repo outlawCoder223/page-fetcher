a3c15c1996f8cd3baf494d05a17d73132ac2fa16
Rance Campbell <rancewcampbell@gmail.com>
2020-04-09 01:58:22 +0000

    fetcher takes HTTP GET request and writes it to file specified in command line arg

--- fetcher.js
+++ fetcher.js
@@ -1,10 +1,15 @@
 const request = require('request');
+const fs = require('fs');
 
 const url = process.argv[2];
 const path = process.argv[3];
 
 request(url, (error, response, body) => {
-  console.log('error: ', error);
-  console.log('statusCode:', response && response.statusCode);
-  console.log('body:', body);
+  fs.writeFile(path, body, (err) => {
+    if (err) return console.log(err);
+    const stats = fs.statSync(path);
+    const fileSize = stats['size'];
+    console.log(`Downloaded and saved ${fileSize} bytes to ${path}`);
+  });
 });
+
