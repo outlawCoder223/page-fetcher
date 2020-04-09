4c9b0fa4edf819c1f1c76817b48481033f059433
Rance Campbell <rancewcampbell@gmail.com>
2020-04-09 02:19:51 +0000

    if the request fails, the file will not be written and the error and response code are logged

--- fetcher.js
+++ fetcher.js
@@ -5,11 +5,18 @@ const url = process.argv[2];
 const path = process.argv[3];
 
 request(url, (error, response, body) => {
-  fs.writeFile(path, body, (err) => {
-    if (err) return console.log(err);
-    const stats = fs.statSync(path);
-    const fileSize = stats['size'];
-    console.log(`Downloaded and saved ${fileSize} bytes to ${path}`);
-  });
+  
+  if (!error && response.statusCode < 300) {
+    fs.writeFile(path, body, (err) => {
+      if (err) return console.log(err);
+      const stats = fs.statSync(path);
+      const fileSize = stats['size'];
+      console.log(`Downloaded and saved ${fileSize} bytes to ${path}`);
+    });
+  } else {
+    console.log('Error: ', error);
+    console.log('Response Code: ', response.statusCode);
+  }
+  
 });
 
